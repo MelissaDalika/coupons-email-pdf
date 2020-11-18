6fd9170a716e0a18539c8fa53504d69d7366306b
Melissa Lopez <melissa.melisita@gmail.com>
2020-11-18 18:12:23 +0100

    Config MongoDB Atlas Database

---
+++ backend/database/db-atlas.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to Mongo!');
+    })
+    .catch((err) => {
+        console.error('Error connecting to Mongo', err);
+    });
